46f1b2724a5a694fcd802280b24d57cff831604a
sklim <dra1125@hanmail.net>
2016-12-30 23:14:06 +0900

    add photo model and test

---
+++ photo/models.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+from django.db import models
+
+# Create your models here.
+class Photo(models.Model):
+    image_file = models.ImageField(upload_to='static_files/uploaded/original/%Y/%m/%d')
+    filtered_image = models.ImageField(upload_to='static_files/uploaded/filtered/%Y/%m/%d')
+    description = models.TextField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def delete(self, *args, **kwargs):
+        self.image_file.delete()
+        self.filtered_image.delete()
+        super(Photo, self).delete(*args, **kwargs)
