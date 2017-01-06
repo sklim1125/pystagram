3c520fe8be3c0d15043d9fc115115d3ee3994459
sklmi <dra1125@hanmail.net>
2017-01-06 15:14:11 +0900

    Add Single Photo View func.

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# coding: utf-8
+
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Photo
 
 # Create your views here.
+def single_photo(request, photo_id):
+#    try:
+#        photo = Photo.objects.get(pk=photo_id)
+#    except Photo.DoesNotExist:
+#        return HttpResponse("사진이 없습니다.")
+
+    photo = get_object_or_404(Photo, pk=photo_id)
+    response_text = '<p>{photo_id}번 사진을 보여줍니다.</p>'
+    response_text += '<p>{photo_url}</p>'
+    response_text += '<p><img src="{photo_url}" /></p>'
+
+    return HttpResponse(response_text.format(photo_id=photo_id, photo_url=photo.image_file.url))
--- pystagram/urls.py
+++ pystagram/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from photo import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^photo/(?P<photo_id>\d+)$', views.single_photo, name='view_single_photo'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
